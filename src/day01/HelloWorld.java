28ce0bb146c015807705136e73a16c7ff1680801
zhakekaidiya <82983090+zhakekaidiya@users.noreply.github.com>
2021-04-24 16:52:56 -0400

    added a line after publishing to GitHub

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("adding github now");
 
+        System.out.println("yay this thing is now on GitHub");
     }
 }
